591eca4d4f57111b96775c1baabf2b6d1f9afbf4
carlos.junior <carlos.junior@uds.com.br>
2022-04-12 23:01:03 -0300

    entidade servidor

--- Servidor.py
+++ Servidor.py
@@ -1,6 +1,5 @@
-from http import server
+from re import S
 import socket
-from time import sleep
 
 class Servidor:
 
@@ -11,6 +10,24 @@ class Servidor:
         self.servidor_socket = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
         #Configuração para permitir que um endereço de IP e uma porta possam ser reutilizados no caso de um crash da aplicação
         self.servidor_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    def alterar_status_lixeira(self,mensagem):
+        print("entrou no método certo.")
+        print('\n')
+        print(mensagem)
+        print('\n')
+
+    def dados_das_lixeiras(self,mensagem):
+        print(mensagem)
+
+    def esvaziar_lixeira(self, mensagem):
+        print(mensagem)
+
+    def alterar_ordem_lixeiras(self, mensagem):
+        print(mensagem)
+
+    def adicionar_lixo_lixeira(self, mensagem):
+        print(mensagem)
 
     def iniciar_servidor(self, ip, porta):
         #Incializando o servidor
@@ -22,27 +39,40 @@ class Servidor:
             self.servidor_socket.listen(5)
 
     def receber_mensagem(self):
-        self.iniciar_servidor('192.168.43.56', 2004)
-        i = 0
+        self.iniciar_servidor("192.168.25.5", 2004)
         while True:
-            print("Aguardando conexão.")
-            sleep(2)
+            print("Aguardando conexão.")            
             #Aceita a conexão de um cliente
-            cliente, endereço = self.servidor_socket.accept()
-            print("conexão efetuada com sucesso com o endereço ", endereço)
+            cliente, endereco = self.servidor_socket.accept()
+            print("conexão efetuada com sucesso com o endereço "+str(endereco)+".")
             #Recebe os dados enviados pelo cliente, até o limite do payload em bytes
             dados = cliente.recv(self.payload)
             if dados:
+                mensagem = dados.decode('utf-8')
+                if mensagem.split('/')[0] == 'alterar status':
+                    self.alterar_status_lixeira(mensagem)
+                elif mensagem.split('/')[0] == "dados das lixeiras":
+                    self.dados_das_lixeiras(mensagem)
+                elif mensagem.split('/')[0] == "esvaziar lixeira":
+                    self.esvaziar_lixeira(mensagem)
                 #Caso haja dados, eles são printados no terminal
-                print("Dados: %s" %dados)
-                #Envia os dados recebidos de volta para o cliente
-                cliente.send(dados)
-                print("Enviado os dados recebidos de volta ao cliente com endereço ",endereço,".")
+                print("Dados: %s" %dados.decode('utf-8'))
                 #Fecha a conexão
                 cliente.close()
-                i +=1
-            if i == 3:
-                break
+
+    
+    def enviar_mensagem(self, mensagem):
+        try: 
+            #Tenta enviar uma mensagem
+            self.servidor_socket.send(bytes(mensagem,'utf-8'))
+        except socket.error as e: 
+            print ("Socket error: ",str(e)) 
+        except Exception as e: 
+            print ("Ocorreu uma exceção:  ",str(e)) 
+        finally: 
+            print ("Fechando a conexão com o servidor")
+            #Finaliza a conexão com o servidor
+            self.servidor_socket.close() 
 
 if __name__ == "__main__":
     servidor = Servidor()
