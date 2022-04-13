e00aa29ee20ebbda5b38455dbd2a2210500ecf50
carlos.junior <carlos.junior@uds.com.br>
2022-04-12 23:00:53 -0300

    entidade lixeira

--- Lixeira.py
+++ Lixeira.py
@@ -1,3 +1,8 @@
+import imp
+
+
+import socket
+
 class Lixeira:
 
     def __init__(self):
@@ -6,6 +11,14 @@ class Lixeira:
         self.longitude_lixeira = 0
         self.carga_lixeira = 0
         self.status_lixeira = "aberta"
+        self.cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+    def lixeira_conectar(self,ip, porta):
+        #Tenta estabelecer uma conexão com o endereço de IP e a porta informados
+        print("Se conectando ao ip ", ip," na porta ",porta,".")
+        endereco = (ip, porta)
+        self.cliente_socket.connect(endereco)
 
     def definir_capacidade(self, capacidade_lixeira):
         self.capacidade_lixeira = capacidade_lixeira
@@ -30,3 +43,7 @@ class Lixeira:
 
     def status_da_lixeira(self):
         return self.status_lixeira
+
+if __name__ == "__main__":
+    lixeira = Lixeira()
+    lixeira.lixeira_conectar("192.168.43.143", 7777)
