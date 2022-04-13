e6cdabc1caddbd5781544ce23185de6771c30f7c
carlosajunior <carlosajunior@hotmail.com>
2022-04-13 20:15:30 -0300

    adicionado metodo de alterar status

--- Client.py
+++ Client.py
@@ -4,7 +4,6 @@ class Cliente:
     def __init__(self):
         #Configuração do socket para atender ao protocolo TCP/IP
         self.cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     
     def cliente_conectar(self,ip, porta):
         #Tenta estabelecer uma conexão com o endereço de IP e a porta informados
@@ -13,11 +12,9 @@ class Cliente:
         self.cliente_socket.connect(endereco)
 
     def cliente_enviar(self,mensagem):
-        try: 
-            #Tenta enviar uma mensagem
-            self.cliente_conectar("192.168.25.5", 2004)
-            while True:
-                self.cliente_socket.send(bytes("alterar status/Teste",'utf-8'))
+        try:             
+            self.cliente_socket.send(bytes("alterar status/Teste",'utf-8'))
+            response = self.cliente_socket.recv(2048)
         except socket.error as e: 
             print ("Socket error: ",str(e)) 
         except Exception as e: 
@@ -25,12 +22,18 @@ class Cliente:
         finally: 
             print ("Fechando a conexão com o servidor")
             #Finaliza a conexão com o servidor
-            self.cliente_socket.close() 
+            self.cliente_socket.close()
+            return response.decode('utf-8') 
 
     def alterar_status_lixeira(self,latitude, longitude, status):
         mensagem = 'alterar status'+'/'+latitude+'/'+longitude+'/'+status
-        self.cliente_enviar(self, mensagem)
+        response = self.cliente_enviar(self, mensagem)
+        if response == 'status alterado':
+            print('status alterado com sucesso.')
+        else:
+            print('ocorreu um erro ao tentar alterar o status.')
 
+    def alterar_percurso(self):
 if __name__ == "__main__":
     cliente = Cliente()
     cliente.cliente_enviar("Teste")
