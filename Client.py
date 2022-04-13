3cf53039f99b13ea75fe01335123eece6b7754f9
carlos.junior <carlos.junior@uds.com.br>
2022-04-12 23:00:44 -0300

    entidade client

--- Client.py
+++ Client.py
@@ -1,4 +1,3 @@
-from calendar import c
 import socket
 
 class Cliente:
@@ -9,32 +8,30 @@ class Cliente:
     
     def cliente_conectar(self,ip, porta):
         #Tenta estabelecer uma conexão com o endereço de IP e a porta informados
-        print("Se conectando ao ip ", ip," na porta ",porta,".")
+        print("Se conectando ao ip "+ip+" na porta "+str(porta)+".")
         endereco = (ip, porta)
         self.cliente_socket.connect(endereco)
 
-def cliente_enviar(self,mensagem):
-    try: 
-        #Tenta enviar uma mensagem
+    def cliente_enviar(self,mensagem):
+        try: 
+            #Tenta enviar uma mensagem
+            self.cliente_conectar("192.168.25.5", 2004)
+            while True:
+                self.cliente_socket.send(bytes("alterar status/Teste",'utf-8'))
+        except socket.error as e: 
+            print ("Socket error: ",str(e)) 
+        except Exception as e: 
+            print ("Ocorreu uma exceção:  ",str(e)) 
+        finally: 
+            print ("Fechando a conexão com o servidor")
+            #Finaliza a conexão com o servidor
+            self.cliente_socket.close() 
 
-        self.cliente_socket.sendall(mensagem.encode('utf-8')) 
-        # Look for the response 
-        quantidade_recebida = 0 
-        quantidade_enviada = len(mensagem)
-        #Verifica se a quantidade de bytes recebidas é correspondente ao tamanho da mensagem enviada 
-        while quantidade_recebida < quantidade_enviada: 
-            dados = self.cliente_socket.recv(16) 
-            quantidade_recebida += len(dados) 
-            print ("Recebido: ",dados) 
-    except socket.error as e: 
-        print ("Socket error: ",str(e)) 
-    except Exception as e: 
-        print ("Ocorreu uma exceção:  ",str(e)) 
-    finally: 
-        print ("Fechando a conexão com o servidor")
-        #Finaliza a conexão com o servidor
-        self.cliente_socket.close() 
+    def alterar_status_lixeira(self,latitude, longitude, status):
+        mensagem = 'alterar status'+'/'+latitude+'/'+longitude+'/'+status
+        self.cliente_enviar(self, mensagem)
 
 if __name__ == "__main__":
     cliente = Cliente()
-    cliente.cliente_conectar("192.168.43.143", 7777)
+    cliente.cliente_enviar("Teste")
+    
