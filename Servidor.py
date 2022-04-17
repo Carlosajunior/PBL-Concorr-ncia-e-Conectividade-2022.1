6f2ad4a8f3608f5b4954908a4820788f37ae4c21
carlosajunior <carlosajunior@hotmail.com>
2022-04-17 18:49:59 -0300

    implementação do multi threading

--- Servidor.py
+++ Servidor.py
@@ -1,9 +1,10 @@
-from re import S
 import socket
+import threading
 
 class Servidor:
 
     def __init__(self):
+        self.lista_de_clientes_conectados = []        
         #Define o tamanho máximo de bytes que podem ser recebidos
         self.payload = 2048
         #Configuração do socket para seguir o protocolo TCP/IP
@@ -11,40 +12,49 @@ class Servidor:
         #Configuração para permitir que um endereço de IP e uma porta possam ser reutilizados no caso de um crash da aplicação
         self.servidor_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     
+        
     def alterar_status_lixeira(self,mensagem):
-        print("entrou no método certo.")
-        print('\n')
-        print(mensagem)
-        print('\n')
+        response = self.enviar_mensagem(self, mensagem)
 
     def dados_das_lixeiras(self,mensagem):
-        print(mensagem)
+        pass
 
     def esvaziar_lixeira(self, mensagem):
-        print(mensagem)
+        pass
 
     def alterar_ordem_lixeiras(self, mensagem):
-        print(mensagem)
+        pass
 
     def adicionar_lixo_lixeira(self, mensagem):
-        print(mensagem)
+        pass
 
-    def iniciar_servidor(self, ip, porta):
-        #Incializando o servidor
-            print("Iniciando o servidor no IP ",ip," e na porta ",porta,".")
-            endereco = (ip,porta)
+    def alterar_trajeto_caminhao(self, mensagem):
+        pass
+        
+    def iniciar_servidor(self):
+        #Incializando o servidor            
+            endereco = ('localhost',2004)
+            print("Iniciando o servidor no localhost na porta 2004.")
             print("Servidor rodando no endereço ", endereco)
             self.servidor_socket.bind(endereco)
-            #Servidor a passar a aguarda por conexões de clientes, num máximo de 5 clientes em uma fila de espera para se conectar
-            self.servidor_socket.listen(5)
+            #Servidor a passar a aguarda por conexões de clientes, num máximo de 10 clientes em uma fila de espera para se conectar
+            self.servidor_socket.listen(10)
+            print("Aguardando conexão.")
+            while True:
+                #Aceita a conexão de um cliente
+                cliente, endereco = self.servidor_socket.accept()
+                print("conexão efetuada com sucesso com o endereço "+str(endereco)+".")
+                #Adiciona o cliente conectado a uma lista de clientes conectados
+                self.lista_de_clientes_conectados.append(cliente)
+                #Incia uma thread para receber mensagens do cliente que se conectou
+                thread = threading.Thread(target= self.receber_mensagem, args = [cliente])
+                thread.start()        
 
-    def receber_mensagem(self):
-        self.iniciar_servidor("192.168.25.5", 2004)
-        while True:
-            print("Aguardando conexão.")            
-            #Aceita a conexão de um cliente
-            cliente, endereco = self.servidor_socket.accept()
-            print("conexão efetuada com sucesso com o endereço "+str(endereco)+".")
+    def remover_cliente(self, cliente):
+        self.lista_de_clientes_conectados.remove(cliente)
+    
+    def receber_mensagem(self,cliente):                 
+        try:
             #Recebe os dados enviados pelo cliente, até o limite do payload em bytes
             dados = cliente.recv(self.payload)
             if dados:
@@ -55,12 +65,11 @@ class Servidor:
                     self.dados_das_lixeiras(mensagem)
                 elif mensagem.split('/')[0] == "esvaziar lixeira":
                     self.esvaziar_lixeira(mensagem)
-                #Caso haja dados, eles são printados no terminal
-                print("Dados: %s" %dados.decode('utf-8'))
-                #Fecha a conexão
-                cliente.close()
+                elif mensagem.split('/')[0] == "alterar trajeto":
+                    self.alterar_trajeto_caminhao(mensagem)
+        except:
+            self.remover_cliente(cliente)
 
-    
     def enviar_mensagem(self, mensagem):
         try: 
             #Tenta enviar uma mensagem
@@ -76,4 +85,4 @@ class Servidor:
 
 if __name__ == "__main__":
     servidor = Servidor()
-    servidor.receber_mensagem()
+    servidor.iniciar_servidor()
