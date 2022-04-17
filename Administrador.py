cbde7f506bff6d0ea7ef4d01ea1845c6c4574aaf
carlosajunior <carlosajunior@hotmail.com>
2022-04-17 15:57:03 -0300

    método pra alterar posição da lixeira na coleta

--- Administrador.py
+++ Administrador.py
@@ -1,20 +1,20 @@
 import socket
 
-class Cliente:
+class Administrador:
     def __init__(self):
         #Configuração do socket para atender ao protocolo TCP/IP
-        self.cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.administrador_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    def cliente_conectar(self,ip, porta):
+    def administrador_conectar(self,ip, porta):
         #Tenta estabelecer uma conexão com o endereço de IP e a porta informados
         print("Se conectando ao ip "+ip+" na porta "+str(porta)+".")
         endereco = (ip, porta)
-        self.cliente_socket.connect(endereco)
+        self.administrador_socket.connect(endereco)
 
-    def cliente_enviar(self,mensagem):
+    def administrador_enviar(self,mensagem):
         try:             
-            self.cliente_socket.send(bytes("alterar status/Teste",'utf-8'))
-            response = self.cliente_socket.recv(2048)
+            self.administrador_socket.send(bytes("alterar status/Teste",'utf-8'))
+            response = self.administrador_socket.recv(2048)
         except socket.error as e: 
             print ("Socket error: ",str(e)) 
         except Exception as e: 
@@ -22,19 +22,25 @@ class Cliente:
         finally: 
             print ("Fechando a conexão com o servidor")
             #Finaliza a conexão com o servidor
-            self.cliente_socket.close()
+            self.administrador_socket.close()
             return response.decode('utf-8') 
 
     def alterar_status_lixeira(self,latitude, longitude, status):
         mensagem = 'alterar status'+'/'+latitude+'/'+longitude+'/'+status
-        response = self.cliente_enviar(self, mensagem)
+        response = self.administrador_enviar(self, mensagem)
         if response == 'status alterado':
             print('status alterado com sucesso.')
         else:
             print('ocorreu um erro ao tentar alterar o status.')
 
-    def alterar_percurso(self):
+    def alterar_percurso(self, latitude, longitude, posicao):
+        mensagem = 'alterar trajeto'+'/'+latitude+'/'+longitude+'/'+posicao
+        response = self.administrador_enviar(self, mensagem)
+        if response == 'posição da lixeira alterada':
+            print('posição da lixeira alterada com sucesso.')
+        else:
+            print(response)
 if __name__ == "__main__":
-    cliente = Cliente()
-    cliente.cliente_enviar("Teste")
+    administrador = Administrador()
+    administrador.administrador_enviar("Teste")
     
