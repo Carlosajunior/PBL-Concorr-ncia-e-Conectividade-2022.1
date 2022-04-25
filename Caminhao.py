61b685020881e71ce5c38baeedff5adcd6677592
carlosajunior <carlosajunior@hotmail.com>
2022-04-25 01:56:07 -0300

    método para esvaziar lixeira

--- Caminhao.py
+++ Caminhao.py
@@ -28,7 +28,9 @@ class Caminhao:
         if dados:
                 mensagem = dados.decode('utf-8')   
                 if mensagem.split('/')[0] == "alterar trajeto":
-                    self.alterar_trajeto_caminhao(mensagem) 
+                    self.alterar_trajeto_caminhao(mensagem)
+                elif mensagem.split('/')[0] == "trajeto das lixeiras":
+                    self.percurso_das_lixeiras()
 
     def alterar_trajeto_caminhao(self, mensagem):
         old_index = mensagem.split('/')[1]
@@ -43,19 +45,34 @@ class Caminhao:
             self.lista_lixeiras.insert(new_index,self.lista_lixeiras.pop(old_index))
             self.enviar_mensagem('posição da lixeira alterada com sucesso.')
 
-    def definir_percurso_lixeiras(self,ip,porta):
-        response = self.caminhao_enviar(ip, porta,"percurso das lixeiras/")
-        self.lista_lixeiras = response.get("lista_lixeiras")
+    def definir_percurso_lixeiras(self):
+        response = self.caminhao_enviar("dados das lixeiras/")
+        mensagem = response.decode('utf-8')
+        if mensagem == 'não há lixeira cadastradas.':
+            print("Não há lixeiras cadastradas no sistema.")
+        else:
+            lixeiras_json = json.loads(mensagem)
+            self.lista_lixeiras = lixeiras_json.get('dados')
+            print('percurso das lixeiras obtido.')
 
     def percurso_das_lixeiras(self):
-        return self.lista_lixeiras
+        percuso_lixeiras = {"dados":self.lista_lixeiras}
+        trajeto_lixeiras = json.dumps(percuso_lixeiras)
+        self.enviar_mensagem(trajeto_lixeiras)
 
-    def esvaziar_lixeira(self,ip,porta):
-        response = self.caminhao_enviar(ip, porta,"esvaziar lixeira/")
-        if response.get("lixeira_response") == 'ok':
-            self.lista_lixeiras.pop()
-            print("lixeira esvaziada.")
-        print("Ocorreu um erro ao tentar esvaziar a lixeira.")
+    def esvaziar_lixeira(self):
+        dados_lixeira = self.lista_lixeiras.pop(0)
+        if dados_lixeira.get('status') == 'aberta':
+            latitude = dados_lixeira.get('posicao').split(',')[0]
+            longitude = dados_lixeira.get('posicao').split(',')[0]
+            response = self.enviar_mensagem('esvaziar lixeira/'+latitude+'/'+longitude)
+            if response.decode('utf-8') == 'lixeira esvaziada':
+                print("lixeira esvaziada.")
+            else:
+                print("Não foi possivel esvaziar a lixeira.")
+        else:
+            print("A lixeira na posicao ",dados_lixeira.get('posicao')," está bloqueada.")
+
 
 
 if __name__ == "__main__":
